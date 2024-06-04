a35cc32ba77633aef9e5f0af47806be31d529ba1
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-06-04 19:03:00 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const { createTodo } = require("./types");
 const { updateTodo } = require("./types");
 
+const { todo } = require("./db");
+
+
 
 const app = express();
 port = 8080
 
 app.use(express.json());
 
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload) {
@@ -18,22 +21,48 @@ app.post('/todo', (req, res) => {
         return;
     }
     //put it in mongo db
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+
+    })
+
+    res.json({
+        msg: " todo created "
+    })
 })
 
-app.get('/todos', (req, res) => {
+app.get('/todos', async (req, res) => {
 
+
+    const todo = await todo.find({});
+
+    res.json({
+        todo
+    })
     
 })
 
-app.put(("/completed", (req, res) => {
+app.put(("/completed", async (req, res) => {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if (!parsedPayload.success) {
         res.status(411).json({
-            msg: "You sent the wrong inpurts".
+            msg: "You sent the wrong inpurts"
         })
         return;
     }
+
+    await todo.update({
+        _id: req.body.id,
+    }, {
+        completed: true
+    })
+
+    res.json({
+        msg: "TODO marked as complete"
+    })
     
 }))
 
